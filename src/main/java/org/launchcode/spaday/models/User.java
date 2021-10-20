6b5c53cd4113c29f2d1645c6f0ed59be871ee8e0
unknown <kevinaborders@gmail.com>
2021-10-19 20:50:43 -0500

    spa studio part 3

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,9 +1,24 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 5, max = 15, message = "Username must be between 5 and 15 characters")
     private String username;
+
+    @Email(message = "Invalid email")
     private String email;
+
+    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
+
+    @NotNull(message = "Passwords do not match")
+    private String verifyPassword;
 
     public User() {
 
@@ -38,5 +53,23 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
+    }
+
+    private void checkPassword() {
+        if (this.password != null && this.verifyPassword != null) {
+            if(!this.password.equals(this.verifyPassword)) {
+                setVerifyPassword(null);
+            }
+        }
     }
 }
